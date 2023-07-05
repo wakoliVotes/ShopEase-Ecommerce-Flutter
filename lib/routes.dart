9a2dc3345b38f399bef38d76027c72b103f13d33
wakolivotes <votesdw@yahoo.com>
2023-07-05 07:50:36 +0300

    routes

---
+++ lib/routes.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+
+
+
+
+// We use name route
+// All our routes will be available here
+
+
+final Map<String, WidgetBuilder> routes = {
+  /*
+  SplashScreen.routeName: (context) => SplashScreen(),
+  SignInScreen.routeName: (context) => SignInScreen(),
+  ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
+  LoginSuccessScreen.routeName: (context) => LoginSuccessScreen(),
+  SignUpScreen.routeName: (context) => SignUpScreen(),
+  CompleteProfileScreen.routeName: (context) => CompleteProfileScreen(),
+  OtpScreen.routeName: (context) => OtpScreen(),
+  HomeScreen.routeName: (context) => HomeScreen(),
+  */
+};
