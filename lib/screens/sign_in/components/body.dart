f4ad4ad77cf4e9b9fcad730ca1817b774e51f4e7
wakolivotes <votesdw@yahoo.com>
2023-07-13 16:18:34 +0300

    Sig In implementation

---
+++ lib/screens/sign_in/components/body.dart
@@ -0,0 +1,13 @@
+
+
+
+import 'package:flutter/cupertino.dart';
+
+class Body extends StatefulWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: child
+    )
+  }
+}
