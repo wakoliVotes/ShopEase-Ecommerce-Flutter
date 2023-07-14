9a334e56f84dd54eb26ba5f52115ee954026de42
wakolivotes <votesdw@yahoo.com>
2023-07-14 16:42:36 +0300

    login success

--- lib/screens/login_success/login_success_screen.dart
+++ lib/screens/login_success/login_success_screen.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shopease_ecommerce/screens/login_success/components/body.dart';
 
 class LoginSuccess extends StatelessWidget {
+  const LoginSuccess({super.key});
+
   @override
   Widget build (BuildContext context) {
     return Scaffold(
