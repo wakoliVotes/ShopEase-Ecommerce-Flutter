7fab846e831bc41d6ca1b7ee7448a0a4c547f157
wakolivotes <votesdw@yahoo.com>
2023-07-14 17:09:40 +0300

    forgot passwordg

--- lib/screens/forgot_password/forgot_password_screen.dart
+++ lib/screens/forgot_password/forgot_password_screen.dart
@@ -1,124 +1,22 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shopease_ecommerce/constants.dart';
+import 'package:shopease_ecommerce/screens/forgot_password/components/body.dart';
 import 'package:shopease_ecommerce/size_config.dart';
 
-class Body extends StatelessWidget {
+class ForgotPasswordScreen extends StatelessWidget {
+
+  const Body({super.key});
+
+  static String routeName = '/forgot_password';
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
-          child: Column(
-            children: [
-              SizedBox(
-                height: SizeConfig.screenHeight * 0.04,
-              ),
-              Text(
-                'Forgot Password',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: getProportionateScreenWidth(28),
-                  color: Colors.black
-                ),
-              ),
-              const Text(
-                'Please ebter your email and we will send \nyou a link to return to your account',
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(
-                height: SizeConfig.screenHeight * 0.1,
-              ),
-              ForgotPassForm(),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+            'Forgot Password'
         ),
       ),
+      body: Body(),
     );
   }
-}
-
-class ForgotPassForm extends StatefulWidget {
-  @override
-  _ForgotPassFormState createState() => _ForgotPassFormState();
-}
-
-class _ForgotPassFormState extends State<ForgotPassForm> {
-  final _formKey = GlobalKey<FormState>();
-
-  List<String> errors = [];
-  String? email;
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            onSaved: (newValue) => email = newValue,
-            onChanged: (value) {
-              if (value.isNotEmpty && errors.contains(kEmailNullError)) {
-                setState(() {
-                  errors.remove(kEmailNullError);
-                });
-              } else if (emailValidatorRegExp.hasMatch(value) &&
-              errors.contains(kInvalidEmailError)
-              ) {
-                setState(() {
-                  errors.remove(kInvalidEmailError)
-                });
-              }
-              return null;
-            },
-            validator: (value) {
-              if (value!.isEmpty && !errors.contains(kEmailNullError) ) {
-                setState(() {
-                  errors.add(kEmailNullError);
-                });
-              } else if (!emailValidatorRegExp.hasMatch(value) &&
-              !errors.contains(kInvalidEmailError)
-              ) {
-                setState(() {
-                  errors.add(kInvalidEmailError);
-                });
-              }
-              return null;
-            },
-            decoration: const InputDecoration(
-              labelText: 'Email',
-              hintText: 'Enter your email',
-              floatingLabelBehavior: FloatingLabelBehavior.always,
-              suffixIcon: Icon(
-                Icons.mail,
-              )
-            ),
-          ),
-          SizedBox(
-            height: getProportionateScreenHeight(30),
-          ),
-          FormError(errors: errors),
-          SizedBox(
-            height: SizeConfig.screenHeight * 0.1,
-          ),
-          OutlinedButton(
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  // TODo what you want here
-                }
-              },
-              child: const Text(
-                'Continue'
-              )
-          ),
-          SizedBox(
-            height: SizeConfig.screenHeight * 0.1,
-          ),
-          NoAccountText(),
-        ],
-      ),
-    );
-  }
-}
