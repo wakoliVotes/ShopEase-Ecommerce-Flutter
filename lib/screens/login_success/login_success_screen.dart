6b4c5d23b5ed7ae12d8fab8827194f678a6ab51a
wakolivotes <votesdw@yahoo.com>
2023-07-14 16:42:01 +0300

    login success

--- lib/screens/login_success/login_success_screen.dart
+++ lib/screens/login_success/login_success_screen.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shopease_ecommerce/screens/login_success/components/body.dart';
 
 class LoginSuccess extends StatelessWidget {
   @override
   Widget build (BuildContext context) {
-
+    return Scaffold(
+      appBar: AppBar(
+        leading: const SizedBox(),
+        title: const Text(
+          'Login Success'
+        ),
+      ),
+      body: Body(),
+    );
   }
 }
