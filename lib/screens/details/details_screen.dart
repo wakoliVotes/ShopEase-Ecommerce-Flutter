b28ecd185346eba9ea77807dff73c97c3a0ed916
wakolivotes <votesdw@yahoo.com>
2023-07-16 06:30:42 +0300

    details details screen

---
+++ lib/screens/details/details_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shopease_ecommerce/screens/details/components/custom_app_bar.dart';
+
+class DetailsScreen extends StatelessWidget {
+  static String routeName = '/details';
+
+  @override
+  Widget build (BuildContext context) {
+    final ProductDetailsArguments agrs =
+        ModalRoute.of(context)!.settings.arguments as ProductDetailsArguments;
+
+    return Scaffold(
+      backgroundColor: Color(0xFFF5F6F9),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(AppBar().preferredSize.height),
+        child: CustomAppBar(rating: agrs.product.rating),
+      ),
+    )
+  }
+}
