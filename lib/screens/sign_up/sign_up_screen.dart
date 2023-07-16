be7bc6f3e9b3e195bde2ca23b9ce60f0c64d3e46
wakolivotes <votesdw@yahoo.com>
2023-07-16 07:13:10 +0300

    Sign Up

---
+++ lib/screens/sign_up/sign_up_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shopease_ecommerce/screens/sign_up/components/body.dart';
+
+class SignUpScreen extends StatelessWidget {
+  static String routeName = '/sign_up';
+
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Sign Up'
+        ),
+      ),
+      body: const Body(),
+    );
+  }
+}
