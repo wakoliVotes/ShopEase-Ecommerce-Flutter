3409c4c41932dfa7e04ca31133503eb4d7593f8b
wakolivotes <votesdw@yahoo.com>
2023-07-17 20:55:07 +0300

    routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/cupertino.dart';
+import 'package:shopease_ecommerce/screens/complete_profile/complete_profile_screen.dart';
+import 'package:shopease_ecommerce/screens/forgot_password/forgot_password_screen.dart';
+import 'package:shopease_ecommerce/screens/home/home_screen.dart';
+import 'package:shopease_ecommerce/screens/login_success/login_success_screen.dart';
+import 'package:shopease_ecommerce/screens/otp/otp_screen.dart';
+import 'package:shopease_ecommerce/screens/sign_in/sign_in_screen.dart';
+import 'package:shopease_ecommerce/screens/sign_up/sign_up_screen.dart';
+import 'package:shopease_ecommerce/screens/splash/splash_screen.dart';
 
-// We use name route
+// Using name route
 // All our routes will be available here
 
 final Map<String, WidgetBuilder> routes = {
-  /*
-  SplashScreen.routeName: (context) => SplashScreen(),
-  SignInScreen.routeName: (context) => SignInScreen(),
-  ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
-  LoginSuccessScreen.routeName: (context) => LoginSuccessScreen(),
-  SignUpScreen.routeName: (context) => SignUpScreen(),
-  CompleteProfileScreen.routeName: (context) => CompleteProfileScreen(),
-  OtpScreen.routeName: (context) => OtpScreen(),
-  HomeScreen.routeName: (context) => HomeScreen(),
-  */
+  SplashScreen.routeName: (context) => const SplashScreen(),
+  SignInScreen.routeName: (context) => const SignInScreen(),
+  ForgotPasswordScreen.routeName: (context) => const ForgotPasswordScreen(),
+  LoginSuccessScreen.routeName: (context) => const LoginSuccessScreen(),
+  SignUpScreen.routeName: (context) => const SignUpScreen(),
+  CompleteProfileScreen.routeName: (context) => const CompleteProfileScreen(),
+  OtpScreen.routeName: (context) => const OtpScreen(),
+  HomeScreen.routeName: (context) => const HomeScreen(),
 };
